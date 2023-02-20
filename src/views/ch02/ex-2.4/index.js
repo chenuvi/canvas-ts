ee9e1f9a662da221e4e1c1306a7713fb7cf7f4be
chenkun <kun.chen@yintech.cn>
2023-02-20 20:23:43 +0800

    Feat: 2.4 放射性渐变

---
+++ src/views/ch02/ex-2.4/index.js
@@ -0,0 +1,31 @@
+/*
+CanvasGradient ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
+x0
+开始圆形的 x 轴坐标。
+
+y0
+开始圆形的 y 轴坐标。
+
+r0
+开始圆形的半径。
+
+x1
+结束圆形的 x 轴坐标。
+
+y1
+结束圆形的 y 轴坐标。
+
+r1
+结束圆形的半径。
+
+*/
+var $ = document.querySelector.bind(document);
+var canvas = $("#canvas"), context = canvas.getContext("2d"), gradient = context.createRadialGradient(canvas.width / 2, canvas.height, 10, canvas.width / 2, 0, 100);
+gradient.addColorStop(0, "blue");
+gradient.addColorStop(0.25, "white");
+gradient.addColorStop(0.5, "purple");
+gradient.addColorStop(0.75, "red");
+gradient.addColorStop(1, "yellow");
+context.fillStyle = gradient;
+context.rect(0, 0, canvas.width, canvas.height);
+context.fill();
