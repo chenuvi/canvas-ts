d93fe2e40c19edd625c78828a9a2e3617d592513
chenkun <kun.chen@yintech.cn>
2023-02-20 20:23:25 +0800

    Feat: 2.3 线性渐变

---
+++ src/views/ch02/ex-2.3/index.ts
@@ -0,0 +1,12 @@
+﻿const $ = document.querySelector.bind(document);
+const canvas = $("#canvas") as HTMLCanvasElement,
+  context = canvas.getContext("2d") as CanvasRenderingContext2D,
+  gradient = context.createLinearGradient(0, 0, 0, canvas.height / 2);
+gradient.addColorStop(0, "blue");
+gradient.addColorStop(0.25, "white");
+gradient.addColorStop(0.5, "purple");
+gradient.addColorStop(0.75, "red");
+gradient.addColorStop(1, "yellow");
+context.fillStyle = gradient;
+context.rect(0, 0, canvas.width, canvas.height);
+context.fill();
